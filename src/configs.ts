e025eb9a5659544af7fe292c1959654a482f5f7e
phamsonha <phamsonha49@gmail.com>
2021-12-02 14:53:33 +0700

    Update configs.ts

--- src/configs.ts
+++ src/configs.ts
@@ -2,7 +2,7 @@ import { GANACHE_NETWORK_ID, KOVAN_NETWORK_ID, RINKEBY_NETWORK_ID, ROPSTEN_NETWO
 import { NetworkSpecificConfigs } from './types';
 
 export const TX_DEFAULTS = { gas: 1000000, gasPrice: 10000000000 };
-export const MNEMONIC = '';
+export const MNEMONIC = 'concert load couple harbor equip island argue ramp clarify fence smart topic';
 export const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
 export const GANACHE_CONFIGS: NetworkSpecificConfigs = {
     rpcUrl: 'http://127.0.0.1:8545',
