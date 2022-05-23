1a77e9446644d387d1a7d6fb1fe7d08c4a40ad8a
hapham <phamsonha49@gmail.com>
2022-05-23 10:27:09 +0700

    update module.sh

--- update_module.sh
+++ update_module.sh
@@ -4,7 +4,7 @@ mv migrations migrations1
 mv contract-addresses contract-addresses1
 mv order-utils order-utils1
 
-ln -s /Users/mac/Works/Solidity/0xProject/protocol/packages/contract-wrappers contract-wrappers
-ln -s /Users/mac/Works/Solidity/0xProject/protocol/packages/migrations migrations
-ln -s /Users/mac/Works/Solidity/0xProject/protocol/packages/contract-addresses contract-addresses
-ln -s /Users/mac/Works/Solidity/0xProject/protocol/packages/order-utils order-utils
+ln -s /Users/phamsonha/Works/blockchain/0xProject/protocol/packages/contract-wrappers contract-wrappers
+ln -s /Users/phamsonha/Works/blockchain/0xProject/protocol/packages/migrations migrations
+ln -s /Users/phamsonha/Works/blockchain/0xProject/protocol/packages/contract-addresses contract-addresses
+ln -s /Users/phamsonha/Works/blockchain/0xProject/protocol/packages/order-utils order-utils
